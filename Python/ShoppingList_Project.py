b8f4f541392735f7fe2a2873e8972c21af16cfc4
Mustafa Ahsan Khan <optaycal@gmail.com>
2024-06-16 00:06:37 +0500

    Made changes to the shopping list project

--- Python/ShoppingList_Project.py
+++ Python/ShoppingList_Project.py
@@ -1,22 +1,35 @@
+def showHelp():
+    print("Enter HELP to show this help message")
+    print("Enter the name of the items you want to buy")
+    print("Enter SHOW to show your current list")
+    print("Enter DONE to finalise your shopping list")
+
 def getNumItems(list):
     return (len(list))
 
-def addItem(list):
-    list.append(input("Enter the item you want to buy: "))
+def addItem(list, temp):
+    list.append(temp)
     print("You now have {} items in your shopping list".format(getNumItems(list)))  # This will print the number of items
 
 def printList(list):
-    print("Your final shopping list is: ")
+    print("Your shopping list is: ")
     temp = 1
     for x in list:
         print(f"{temp}) {x}")  # using f-string
         temp += 1
 
 ShoppingList = []
+showHelp()
 while(1):
-    addItem(ShoppingList)
-    temp = input("Do you want to continue? Y/N: ")
-    if(temp == "N" or temp == "n"):
+    
+    temp = input("Next Input: ")
+    if(temp == "DONE" or temp == "done"):
         break
+    elif(temp == "HELP" or temp == "help"):
+        showHelp()
+    elif(temp == "SHOW" or temp == "show"):
+        printList(ShoppingList)
+    else:
+        addItem(ShoppingList, temp)
 
 printList(ShoppingList)
