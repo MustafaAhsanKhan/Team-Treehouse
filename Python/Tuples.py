048a81d22d9053433d6ce566a184337713d73e14
Mustafa Ahsan <optaycal@gmail.com>
2024-06-16 18:23:52 +0500

    Added tuples

---
+++ Python/Tuples.py
@@ -0,0 +1,11 @@
+# A tuple is similar to a list in every way
+# The only difference is that tuples are Immutable
+# Meaning their contents cant be changed
+# Their size remains constant after initialization
+
+ints = 1,2,3,4,5  # This is a tuple  # The square brackets are removed
+ints = (1,2,3,4,5)  # You can also replace the square brackets with parentheses
+
+print(ints)
+
+int = 1,  # This is a tuple with one element
