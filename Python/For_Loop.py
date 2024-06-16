efe347754137af8a04f269cd8084b183a429c077
Mustafa Ahsan <optaycal@gmail.com>
2024-06-17 03:01:05 +0500

    Finished sequences

--- Python/For_Loop.py
+++ Python/For_Loop.py
@@ -25,3 +25,10 @@ rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 for index, x in enumerate(rainbow, 5):  # This is another method to print lists  # You can even define the starting index
     print(f"{index}) {x}")  # Using f-strings
+
+
+docs = 'Tuples are immutable sequences, typically used to store collections of heterogeneous data (such as the 2-tuples produced by the enumerate() built-in). Tuples are also used for cases where an immutable sequence of homogeneous data is needed (such as allowing storage in a set or dict instance).'
+
+print(docs.count("Tuple"))  # This prints the number of times the string "Tuple" appears in docs
+
+print(docs.index("tuple"))  # This prints the index of the first match of the given string
