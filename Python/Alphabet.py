6a9660a273a3b8f80a0ec1dc285d64ae20cc5ea3
Mustafa Ahsan <optaycal@gmail.com>
2024-06-18 18:40:46 +0500

    Dunder main project

---
+++ Python/Alphabet.py
@@ -0,0 +1,7 @@
+def print_alphabetical(list_p):
+    sorted_list = sorted(list_p)
+    print(sorted_list)
+
+if __name__ == "__main__":
+    word_list = ["hamster", "turtle", "cat", "bird"]
+    print_alphabetical(word_list)
---
+++ Python/DunderMain_Project.py
@@ -0,0 +1,8 @@
+import Alphabet
+
+word_list = ["hamster", "turtle", "cat", "bird"]
+last_name = ["Rodriquez", "Lee", "Khan", "Amanda"]
+
+Alphabet.print_alphabetical(word_list)
+print()
+Alphabet.print_alphabetical(last_name)
