4e887c27f95a431b01cff882b7d6f7f641289120
Mustafa Ahsan <optaycal@gmail.com>
2024-06-23 01:19:13 +0500

    memory game update 2

--- Python/OOP/MemoryGame_Project.py
+++ Python/OOP/MemoryGame_Project.py
@@ -13,16 +13,51 @@ class Game:
             for x in range(1, self.size + 1):
                 self.locations.append(f"{col}{x}")  # This will create the locations eg. A1, B4
 
+    def run(self):
+        self.setCards()
+        win = False
+        while(win == False):
+            self.displayBoard()
+            # Taking input from the user
+            choice1 = input("Enter the first card: ")
+            choice2 = input("Enter the second card: ")
+
+            # Finding the cards in the grid
+            card1 = self.findCard(choice1)
+            card2 = self.findCard(choice2)
+
+            # Checking if the cards are equal
+            if(card1 == card2):
+                print("It's a match!")
+
+            # Checking if all the cards are matched
+            
+            
+        
     def setCards(self):
         for x in self.card_options:
             for y in range(2):
                 random_location = random.choice(self.locations)
                 self.locations.remove(random_location)  # Remove the chosen location
                 self.grid.append(cards.Card(x, random_location))
+
+    def findCard(self, location):
+        for card in self.grid:
+            if card.position == location:
+                return card
                 
+    def displayBoard(self):
+        print("A1   A2   A3   A4")
+        print("-----------------")
+        print("B1   B2   B3   B4")
+        print("-----------------")
+        print("C1   C2   C3   C4")
+        print("-----------------")
+        print("D1   D2   D3   D4")
+        print("-----------------")
 
 if __name__ == "__main__":
     game = Game()
-    game.setCards()
+    game.run()
     for card in game.grid:
         print(f"Card: {card.word}, Location: {card.position}")
