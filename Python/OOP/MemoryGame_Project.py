f9aa4c7334a2c1344889a523d264e9d00a6fa316
Mustafa Ahsan <optaycal@gmail.com>
2024-06-23 01:22:39 +0500

    memory game finished

--- Python/OOP/MemoryGame_Project.py
+++ Python/OOP/MemoryGame_Project.py
@@ -8,6 +8,7 @@ class Game:
         self.card_options = ["Ham", "Egg", "Cat", "Dog", "Log", "Ice", "Bat", "Bag"]
         self.columns = ['A', 'B', 'C', 'D']
         self.locations = []
+        self.revealed = {}  # To track revealed cards
 
         for col in self.columns:
             for x in range(1, self.size + 1):
@@ -16,24 +17,29 @@ class Game:
     def run(self):
         self.setCards()
         win = False
-        while(win == False):
+        while not win:
             self.displayBoard()
             # Taking input from the user
-            choice1 = input("Enter the first card: ")
-            choice2 = input("Enter the second card: ")
+            choice1 = input("Enter the first card (e.g., A1): ")
+            choice2 = input("Enter the second card (e.g., B2): ")
 
             # Finding the cards in the grid
             card1 = self.findCard(choice1)
             card2 = self.findCard(choice2)
 
             # Checking if the cards are equal
-            if(card1 == card2):
-                print("It's a match!")
+            if card1.word == card2.word:
+                print(f"It's a match! You selected {card1.word} and {card2.word}.")
+                self.revealed[choice1] = card1.word
+                self.revealed[choice2] = card2.word
+            else:
+                print(f"Not a match. You selected {card1.word} and {card2.word}. Try again.")
 
             # Checking if all the cards are matched
-            
-            
-        
+            if len(self.revealed) == len(self.card_options) * 2:
+                win = True
+                print("Congratulations! You've matched all the cards!")
+                
     def setCards(self):
         for x in self.card_options:
             for y in range(2):
@@ -47,14 +53,16 @@ class Game:
                 return card
                 
     def displayBoard(self):
-        print("A1   A2   A3   A4")
-        print("-----------------")
-        print("B1   B2   B3   B4")
-        print("-----------------")
-        print("C1   C2   C3   C4")
-        print("-----------------")
-        print("D1   D2   D3   D4")
-        print("-----------------")
+        for col in self.columns:
+            row = []
+            for x in range(1, self.size + 1):
+                loc = f"{col}{x}"
+                if loc in self.revealed:
+                    row.append(self.revealed[loc])
+                else:
+                    row.append(loc)
+            print("   ".join(row))
+            print("-----------------")
 
 if __name__ == "__main__":
     game = Game()
