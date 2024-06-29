200da20d29bc2f4c24e15b22c5093d08dcded9a6
Mustafa Ahsan <optaycal@gmail.com>
2024-06-29 14:23:46 +0500

    finished datetime basics

--- Python/DateAndTime/Basics.py
+++ Python/DateAndTime/Basics.py
@@ -25,3 +25,16 @@ print(d.weekday())  # Monday is 0
 print(d.isoweekday())  # Monday is 1
 
 print(d.isocalendar())  # This is usefull as it gives the week number with respect to the year
+
+# datetime is a class which lets us handle both date and time
+
+dt = datetime.datetime(2024, 7, 10)  # date arguments are required, while the time args are not(default 0)
+print(dt.year)
+print(dt.second)
+
+dt = datetime.datetime.now()  # this is similar to the .today method for date
+
+print(dt)
+
+dt.date()  # this extracts the "date" part
+dt.time()  # this extracts the "time" part
