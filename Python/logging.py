d8755ac5f4f0065c1801b2f2643fcfddbac23108
Mustafa Ahsan <optaycal@gmail.com>
2024-07-29 09:46:25 +0500

    logging

---
+++ Python/logging.py
@@ -0,0 +1,9 @@
+import logging
+
+# critical, error, warning, info, debug, notset
+
+logging.warn("ERROR")
+
+#logging.basicConfig(filename='', level=logging.debug)  # An example how we would log an error to a file
+
+logging.debug("Debug error")
