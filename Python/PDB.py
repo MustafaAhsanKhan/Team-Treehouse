be78ca1c7bebfd99417a30f888ddb9b2efd9156b
Mustafa Ahsan <optaycal@gmail.com>
2024-07-29 09:52:28 +0500

    pdb

--- Python/PDB.py
+++ Python/PDB.py
@@ -4,7 +4,7 @@ my_list = [1, 2, 3, True, "hello", False]
 
 # We need to remove everything that is not numbers
 
-pdb.set_trace() # next/ n to go to next step
+pdb.set_trace() # next/ n to go to next step  # c to exit pdb and continue on
 del my_list[3]
 del my_list[3]
 del my_list[3]
